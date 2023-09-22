2934cd2c8f1acd7fdf8a0f3ed8a67fcb2a7d5959
TanninOne <seppleviathan@gmx.de>
2023-09-22 17:33:14 +0200

    added npm package lock

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,6 @@ const webpack = require('vortex-api/bin/webpack').default;
 
 const config = webpack('modpacks', __dirname, 5);
 config.externals['./build/Release/bsdiff.node'] = './bsdiff.node';
+config.externals['react-markdown'] = 'react-markdown';
 
 module.exports = config;
